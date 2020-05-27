09a18b4c0d8e1ffee8988cada2b10a1e7676def9
Cameron Taggart <cameron.taggart@microsoft.com>
2020-05-27 15:03:08 -0600

    remove async in tests

--- azext_vmware/tests/latest/test_vmware_scenario.py
+++ azext_vmware/tests/latest/test_vmware_scenario.py
@@ -5,76 +5,14 @@
 
 import os
 import unittest
-import asyncio
-import functools
 
-from azure_devtools.scenario_tests import AllowLargeResponse
 from azure.cli.testsdk import (ScenarioTest, ResourceGroupPreparer)
 from azext_vmware.vendored_sdks.models import ApiErrorException
 
-class AsyncScenarioTest(ScenarioTest):
-
-    @staticmethod
-    def await_prepared_test(test_fn):
-        """Synchronous wrapper for async test methods. Used to avoid making changes
-        upstream to AbstractPreparer (which doesn't await the functions it wraps)
-        """
-
-        @functools.wraps(test_fn)
-        def run(test_class_instance, *args, **kwargs):
-            loop = asyncio.get_event_loop()
-            return loop.run_until_complete(test_fn(test_class_instance))
-
-        return run
-
-    async def poll_until_no_exception(self, fn, expected_exception, max_retries=20, retry_delay=3):
-        """polling helper for live tests because some operations take an unpredictable amount of time to complete"""
-
-        for i in range(max_retries):
-            try:
-                fn()
-                break
-            except expected_exception:
-                print("caught %d").format(i)
-                if i == max_retries - 1:
-                    raise
-                if self.is_live:
-                    await asyncio.sleep(retry_delay)
-
-    async def poll_until_exception(self, fn, expected_exception, max_retries=20, retry_delay=3):
-        """polling helper for live tests because some operations take an unpredictable amount of time to complete"""
-
-        for _ in range(max_retries):
-            try:
-                fn()
-                if self.is_live:
-                    await asyncio.sleep(retry_delay)
-            except expected_exception:
-                return
-        self.fail("expected exception {expected_exception} was not raised")
-
-    async def poll_until_result(self, cmd, check_result, max_retries=40, retry_delay=3):
-        for _ in range(max_retries):
-            rslt = cmd()
-            if(check_result(rslt)):
-                return
-            if self.is_live:
-                await asyncio.sleep(retry_delay)
-        self.fail("expected result did not happen")
-
-# Check if a privatecloud operation has succeeded.
-# On create, the state switches from 'Building' to 'Succeeded'.
-# On add and delete authorization from 'updating' to 'Succeeded'.
-def provissioning_succeeded(rslt):
-    state = rslt.get_output_in_json()['provisioningState']
-    # print(state)
-    return state == 'Succeeded'
-
-class VmwareScenarioTest(AsyncScenarioTest):
+class VmwareScenarioTest(ScenarioTest):
 
     @ResourceGroupPreparer(name_prefix='cli_test_vmware')
-    @AsyncScenarioTest.await_prepared_test
-    async def test_vmware(self):
+    def test_vmware(self):
         self.kwargs.update({
             'loc': 'northcentralus',
             'privatecloud': 'cloud1',
@@ -99,12 +37,8 @@ class VmwareScenarioTest(AsyncScenarioTest):
         # count = len(self.cmd('vmware private-cloud list').get_output_in_json())
         # self.assertGreaterEqual(count, 1, 'subscription private cloud count expected to be more than 1')
 
-        # poll until it is no longer ResourceNotFound
-        await self.poll_until_no_exception(lambda: self.cmd('vmware private-cloud show -g {rg} -n {privatecloud}'), ApiErrorException)
-        await self.poll_until_result(lambda: self.cmd('vmware private-cloud show -g {rg} -n {privatecloud}'), provissioning_succeeded)
-
         # get admin credentials
-        # TODO VCFM-2605 not currently supported in test environment
+        # TODO not currently supported in test environment
         # self.cmd('vmware private-cloud listadmincredentials -g {rg} -n {privatecloud}')
 
         # update private cloud to changed default cluster size
@@ -148,9 +82,6 @@ class VmwareScenarioTest(AsyncScenarioTest):
         count = len(self.cmd('vmware private-cloud list -g {rg}').get_output_in_json())
         self.assertEqual(count, 0, 'private cloud count expected to be 0')
 
-        # it should throw ResourceNotFound
-        try:
+        # show should throw ResourceNotFound
+        with self.assertRaisesRegexp(ApiErrorException, 'ResourceNotFound'):
             self.cmd('vmware private-cloud show -g {rg} -n {privatecloud}')
-            raise Exception("private cloud ResourceNotFound expected")
-        except ApiErrorException:
-            pass
